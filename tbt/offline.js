4c443f0b109ae5d0d5a3d30d803b1a3612561c81
superkarlos <50372440+superkarlos@users.noreply.github.com>
2019-05-22 11:45:32 -0300

    Add files via upload

---
+++ tbt/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1558536222,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/naveboa-sheet0.png",
+		"images/btatirar-sheet0.png",
+		"images/bala-sheet0.png",
+		"images/bala-sheet1.png",
+		"images/baixo-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/naveinimiga-sheet0.png",
+		"images/ptvida-sheet0.png",
+		"images/vida-sheet0.png"
+	]
+}
