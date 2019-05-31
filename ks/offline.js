cdea64df18ce2d807892d6fa03770eb9e4285898
superkarlos <50372440+superkarlos@users.noreply.github.com>
2019-05-31 08:37:07 -0300

    Add files via upload

---
+++ ks/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1559302569,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/naveboa-sheet0.png",
+		"images/btatirar-sheet0.png",
+		"images/bala-sheet0.png",
+		"images/bala-sheet1.png",
+		"images/baixo-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/naveinimiga-sheet0.png",
+		"images/botondstroy-sheet0.png",
+		"images/ptvida-sheet0.png",
+		"images/vida-sheet0.png",
+		"images/pp-sheet0.png"
+	]
+}
